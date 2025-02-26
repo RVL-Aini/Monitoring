29081bff1ca98a414dbadddc375dfa4902ab2d80
RVL-Aini <pavel.alekseev.sa@yandex.ru>
2025-02-26 13:17:39 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,4 @@ buildscript {
         mavenCentral()
         gradlePluginPortal()
     }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
-    }
 }

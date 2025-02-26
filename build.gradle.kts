69071edddeb3a106493d3f500480a8379475a608
RVL-Aini <pavel.alekseev.sa@yandex.ru>
2025-02-26 14:25:32 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,23 @@
-allprojects{
-    repositories{
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    kotlin("android") version "1.9.24" apply false
+    id("com.android.application") version "8.1.1" apply false
 }
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-    }
-}
+//allprojects{
+//    repositories{
+//        google()
+//        mavenCentral()
+//        gradlePluginPortal()
+//    }
+//}
+// buildscript {
+//   repositories {
+//        google()
+//        mavenCentral()
+//        gradlePluginPortal()
+//    }
+//}
+//    dependencies {
+//        classpath("com.android.tools.build:gradle:8.1.1")
+//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+//    }
+//}

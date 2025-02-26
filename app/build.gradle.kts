68f52f4f40d1ef181c27453f8dd0bd55b2dddb2d
RVL-Aini <pavel.alekseev.sa@yandex.ru>
2025-02-26 14:19:03 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,5 +30,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
-    implementation("org.jetbrains.kotlin.:kotlin-stdlib:1.9.21")
+    implementation("org.jetbrains.kotlin.:kotlin-stdlib:1.9.24")
 }

d5794e1aa7eea461b998ea0403a5c077c81507de
RVL-Aini <pavel.alekseev.sa@yandex.ru>
2025-02-26 16:02:57 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,11 @@
 plugins {
-    kotlin("android") version "1.9.24" apply false
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application")
+    kotlin("android")
 }
-
 
 android {
     namespace = "com.example.monitoring"
     compileSdk = 34
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
 
     defaultConfig {
         applicationId = "com.example.monitoring"
@@ -30,6 +24,12 @@ android {
             )
         }
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.24"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
 }
